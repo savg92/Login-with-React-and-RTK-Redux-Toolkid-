94c27b8b37407dd72870567b42770921896668bf
savg92 <1992savg@gmail.com>
2023-08-09 19:12:48 -0300

    updated publicRoutes

--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { RootState } from '../app/store'
 
+interface Props {
+  children?: ReactNode;
+}
+
 // this is a component that will be used to protect routes and redirect to user page if user is logged
-const PublicRoutes = ({ children }) => {
+const PublicRoutes : React.FC<Props> = ({ children }: Props) : JSX.Element => {
   const logged = useSelector((state: RootState) => state.user.isLogged);
 
   return logged ? <Navigate to="/user" replace /> : children
